98743fdb988b6d005640051c2b406bb888592fc1
Nimesh Paudel <nimeshpaudel1025@gmail.com>
2019-11-17 18:36:29 +0545

    My calalculator.

---
+++ app/src/main/java/com/calculator/Arithmetic.java
@@ -0,0 +1,49 @@
+package com.calculator;
+
+public class Arithmetic {
+    public Arithmetic(int first, int second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    private int first;
+    private int second;
+
+    public int add() {
+        return first + second;
+    }
+
+    public int sub() {
+        return first - second;
+    }
+
+    public int multiply() {
+        return first * second;
+    }
+
+    public int divide() {
+        return first / second;
+    }
+
+    public String displayAdd()
+    {
+        return first + "+" + second;
+    }
+
+    public String displaySub()
+    {
+        return first + "-" + second;
+    }
+
+    public String displayMultiply()
+    {
+        return first + "*" + second;
+    }
+
+    public String displayDivide()
+    {
+        return first + "/" + second;
+    }
+
+
+}
